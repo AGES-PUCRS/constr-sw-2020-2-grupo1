9efc2483caa2e88507af53a6447f0547021f6ec1
AndrewsSouza <andrews.souza@outlook.com.br>
2020-09-24 12:19:26 -0300

    api funcional basica

---
+++ api/src/domains/entities/professor.ts
@@ -0,0 +1,24 @@
+class Professor {
+    nome: String
+    cdMatricula: String
+    escola: String
+    email: String
+    idProfessor: Number
+    numTelefone: String
+
+    constructor(nome: String,
+        cdMatricula: String,
+        escola: String,
+        email: String,
+        idProfessor: Number,
+        numTelefone: String,
+        ) {
+            this.nome = nome
+            this.cdMatricula = cdMatricula
+            this.escola = escola
+            this.email = email
+            this.numTelefone = numTelefone
+            this.idProfessor = idProfessor
+    }
+
+}
---
+++ api/src/routes/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+const routes = Router()
+
+// routes.get('/professores', professorController.get)
+// routes.delete('/professores/:id', professorController.delete)
+// routes.post('/professores', professorController.create)
+// routes.put('/professores/:id', professorController.get)
+
+export default routes
